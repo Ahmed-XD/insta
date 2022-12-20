368a9268a1b8664b6027c0ee169a86268955cd1c
Ahmed-XD <110410370+Ahmed-XD@users.noreply.github.com>
2022-12-20 19:03:56 +0600

    Create run.py

---
+++ run.py
@@ -0,0 +1,29 @@
+import os, platform, time
+os.system('git pull')
+os.system('clear')
+print('follow my github and star my repo');time.sleep(3)
+os.system('xdg-open https://github.com/Ahmed-XD/insta')
+os.system('clear')
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        import XNX
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        print('your phone is 32 bit')
